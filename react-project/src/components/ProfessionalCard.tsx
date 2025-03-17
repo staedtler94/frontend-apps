f8dd364b41e25ef191e08d10f6b5ee6d57e1d1ca
Siddhesh Dhage <siddheshdhage@yahoo.com>
2025-03-17 20:01:18 +0000

    Update ProfessionalCard.tsx

--- react-project/src/components/ProfessionalCard.tsx
+++ react-project/src/components/ProfessionalCard.tsx
@@ -4,7 +4,7 @@ interface ProfessionalCardProps {
   children: ReactElement;
 }
 
-const ProfessionalCard: React.FC<ProfessionalCardProps> = ({ children: ReactElement, bg = "bg-indigo-100" }) => {
+const ProfessionalCard: React.FC<ProfessionalCardProps> = ({ children, bg = "bg-indigo-100" }) => {
   return <div className={`${bg} p-6 rounded-lg shadow-md`}>{children}</div>;
 }
 
